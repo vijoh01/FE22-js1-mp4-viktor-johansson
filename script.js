faa2766edfb4470587e9176315071589f15c30a5
vijoh01 <76432491+vijoh01@users.noreply.github.com>
2022-12-12 03:34:11 +0100

    klar

--- script.js
+++ script.js
@@ -1,41 +1,70 @@
 
 $(() => {
-    
-    let animation = {
+    let animation = [anime({
         targets: 'path',
         strokeDashoffset: [anime.setDashoffset, 0],
         easing: 'easeInOutSine',
-        duration: 1500,
+        duration: 20000,
         opacity: 0.8,
         delay: 500,
         direction: 'alternate',
         loop: true
-    }
+    }),
 
-    let showLetters = {
+    anime({
         targets: '#letter-container svg',
         translateY: 1000,
+        opacity: 1,
         easing: 'linear',
         delay: anime.stagger(500),
+        scale: 1.1,
         direction: 'alternate',
-        endDelay: 1000,
+        endDelay: 2000,
         loop: true,
-       
-    }
+    }),
 
-    let shrink = {
+    anime({
         targets: '#arrows',
         easing: 'linear',
         direction: 'alternate',
-        translateX: ['-50rem', 0],
-        endDelay: 1000,
+        translateX: ['-42rem', 0],
+        endDelay: 2000,
         delay: 500,
         duration: 3000,
+        easing: 'steps(6)',
         loop: true,
-       
-    }
-    anime(animation);
+    }),
 
-    anime(showLetters);
-    anime(shrink);
+    anime({
+        targets: '#text-container',
+        backgroundColor: 'hsl(100, 40%, 80%)',
+        keyframes: [{ filter: 'drop-shadow(50px 50px 0px rgb(78, 156, 201))' },
+        { filter: 'drop-shadow(-50px -50px 0px rgb(78, 156, 201)) grayscale(80%)' }],
+        ease: 'easeInOutSine',
+        endDelay: 500,
+        delay: 500,
+        duration: 10000,
+        direction: 'alternate',
+        loop: true,
+        pause: true
+    })];
+
+    animation.forEach((ani) => ani.pause());
+    
+    let playing;
+
+    $('#buttons').click((e) => {
+        e.preventDefault();
+        
+        if (e.target.id == "play") {
+            if (!playing) {
+                animation.forEach((ani) => ani.play());
+                return playing = true;
+            }
+            animation.forEach((ani) => ani.pause());
+            return playing = false;
+        }
+        animation.forEach((ani) => { ani.restart(); playing = false; ani.pause() });
+
+    });
 });
